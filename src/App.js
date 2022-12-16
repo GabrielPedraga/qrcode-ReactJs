a4a9475849dc85c896169f70e5c2baf1b6f7a156
GabrielPedraga <107312107+GabrielPedraga@users.noreply.github.com>
2022-12-16 07:05:40 -0300

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,45 @@
+import { useState } from 'react';
+import QRCode from 'react-qr-code';
+import QRcodeLink from 'qrcode';
+
+import './App.css';
+
+function App() {
+  const [link, setLink] = useState('')
+  const [qrcodeLink, setQrcodeLink] = useState('')
+
+function handleGenerate(link_url){
+  QRcodeLink.toDataURL(link_url,{
+    width: 600,
+    margin: 3,
+  }, function(err, url){
+    setQrcodeLink(url);
+  })
+}
+
+function handleQrcode(e){
+  setLink(e.target.value);
+  handleGenerate(e.target.value)
+}
+
+  return (
+    <div className="container">
+
+    <QRCode
+    value={link}
+    />
+
+     <input
+     className="input"
+     placeholder="Digite o seu link..."
+     value={link}
+     onChange={ (e) => handleQrcode(e)}
+     />
+
+     <a href={qrcodeLink} download={'qrcode.png'} >Baixar QrCode</a>
+
+    </div>
+  );
+}
+
+export default App;
